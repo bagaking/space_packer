bc9b29b1fde6ad00239029bcda27f04adb995853
bagaking <zfaustk@qq.com>
2018-09-17 18:32:46 +0800

    feat (lib): add dir encoding, add codeMapper.js

---
+++ test/lib/codeMapper_test.js
@@ -0,0 +1,20 @@
+const CodeMapper = require('../../lib/encoding/codeMapper')
+const assert  = require('assert');
+const should = require('should');
+
+let cm = null
+
+describe('lib/encoding/CodeMapper', function(){
+    beforeEach(async () => {
+        cm = new CodeMapper(null)
+    })
+
+    it('encode', function(){
+        cm.encode(1).should.equal('A')
+    })
+
+    it('encode', function(){
+        cm.decode('A').should.equal(1)
+    })
+})
+
