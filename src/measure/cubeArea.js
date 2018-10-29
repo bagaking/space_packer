7b0f5a7bd3f01df480c2a4b5171a4ad1a50d1c11
bagaking <zfaustk@qq.com>
2018-10-30 01:00:01 +0800

    fix (measure): cubeArea using V3D

--- src/measure/cubeArea.js
+++ src/measure/cubeArea.js
@@ -1,12 +1,12 @@
-const V3Discrete = require('./v3D')
-const V3SizeDiscrete = require('./v3DSize')
+const V3D = require('./v3D')
+const V3DSize = require('./v3DSize')
 
 class CubeArea {
 
     /**
      * Create a cubeArea
-     * @param {V3Discrete} v3Origin
-     * @param {V3SizeDiscrete} v3Size
+     * @param {V3D} v3Origin
+     * @param {V3DSize} v3Size
      */
     constructor(v3Origin, v3Size) {
         this._origin = v3Origin
@@ -15,7 +15,7 @@ class CubeArea {
 
     /**
      * get size
-     * @returns {V3SizeDiscrete|*}
+     * @returns {V3DSize|*}
      */
     get size() {
         return this._size
@@ -23,7 +23,7 @@ class CubeArea {
 
     /**
      * get origin pos
-     * @returns {V3Discrete|*}
+     * @returns {V3D|*}
      */
     get origin() {
         return this._origin
@@ -34,13 +34,13 @@ class CubeArea {
      * @param {number} x
      * @param {number} y
      * @param {number} z
-     * @returns {V3SizeDiscrete}
+     * @returns {V3D}
      */
     posInBox(x, y, z) {
         let pMin = this.origin
-        let pMax = this.origin.add(this.size).sub(V3Discrete.prefab.one)
+        let pMax = this.origin.add(this.size).sub(V3D.prefab.one)
         let m = (min, max, v) => Math.min(max, Math.max(min, v))
-        return new V3Discrete(
+        return new V3D(
             m(pMin.x, pMax.x, x), m(pMin.y, pMax.y, y), m(pMin.z, pMax.z, z)
         )
     }
@@ -50,10 +50,10 @@ class CubeArea {
      * @param {number} x
      * @param {number} y
      * @param {number} z
-     * @return {V3Discrete}
+     * @return {V3D}
      */
     posA2B(x,y,z){
-        return new V3Discrete(x - this.origin.x, y - this.origin.y, z - this.origin.z);
+        return new V3D(x - this.origin.x, y - this.origin.y, z - this.origin.z);
     }
 
     /**
@@ -61,16 +61,16 @@ class CubeArea {
      * @param {number} x
      * @param {number} y
      * @param {number} z
-     * @return {V3Discrete}
+     * @return {V3D}
      */
     posB2A(x,y,z){
-        return new V3Discrete(x + this.origin.x, y + this.origin.y, z + this.origin.z);
+        return new V3D(x + this.origin.x, y + this.origin.y, z + this.origin.z);
     }
 
 
     /**
      * convert absolute pos to index = y * width * depth + x * depth + z
-     * @param {V3SizeDiscrete} pos
+     * @param {V3DSize} pos
      * @returns {number} uint
      */
     posA2Ind(pos) {
@@ -80,7 +80,7 @@ class CubeArea {
     /**
      * convert index to absolute pos
      * @param ind
-     * @returns {V3SizeDiscrete}
+     * @returns {V3DSize}
      */
     ind2PosA(ind) {
         return this.size.ind2Pos(this.origin, ind)
@@ -88,20 +88,20 @@ class CubeArea {
 
     /**
      * convert box pos to index = y * width * depth + x * depth + z
-     * @param {V3SizeDiscrete} pos
+     * @param {V3DSize} pos
      * @returns {number} uint
      */
     posB2Ind(pos) {
-        return this.size.pos2Ind(V3Discrete.prefab.zero, pos);
+        return this.size.pos2Ind(V3D.prefab.zero, pos);
     }
 
     /**
      * convert index to box pos
      * @param ind
-     * @returns {V3SizeDiscrete}
+     * @returns {V3DSize}
      */
     ind2PosB(ind) {
-        return this.size.ind2Pos(V3Discrete.prefab.zero, ind)
+        return this.size.ind2Pos(V3D.prefab.zero, ind)
     }
 
 
